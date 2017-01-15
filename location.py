9dabd73bd94de5791347cb85f078bc80627a1116
EssamB13 <essam.b97@gmail.com>
2017-01-15 13:09:19 -0500

    Add files via upload

---
+++ location.py
@@ -0,0 +1,54 @@
+class Location:
+    def __init__(self, row, column):
+        """Initialize a location.
+
+        @type self: Location
+        @type row: int
+        @type column: int
+        @rtype: None
+        """
+        # TODO
+        self.row = row
+        self.column = column
+
+    def __str__(self):
+        """Return a string representation.
+
+        @rtype: str
+        """
+        # TODO
+        return "The location is on column: {} , row: {}".format(self.column,
+                                                                self.row)
+
+    def __eq__(self, other):
+        """Return True if self equals other, and false otherwise.
+
+        @rtype: bool
+        """
+        # TODO
+        return type(self) == type(other) and self.row == other.row and \
+                   self.column == other.column
+
+
+def manhattan_distance(origin, destination):
+    """Return the Manhattan distance between the origin and the destination.
+
+    @type origin: Location
+    @type destination: Location
+    @rtype: int
+    """
+    return abs(origin.column - destination.column) + abs(origin.row -
+                                                         destination.row)
+
+
+def deserialize_location(location_str):
+    """Deserialize a location.
+
+    @type location_str: str
+        A location in the format 'row,col'
+    @rtype: Location
+    """
+    # TODO
+    location_list = location_str.split(",")
+    deserialized_loc = Location(int(location_list[0]), int(location_list[1]))
+    return deserialized_loc
